4620458177c696306a25b6aaddf36d570130ca3a
sevenio <seveniotacheon@gmail.com>
2018-08-03 10:41:33 +0530

    new test case implemented

--- app/src/main/java/com/example/gangadhar/tipcalculator/model/RestaurantCalculator.kt
+++ app/src/main/java/com/example/gangadhar/tipcalculator/model/RestaurantCalculator.kt
@@ -1,8 +1,13 @@
 package com.example.gangadhar.tipcalculator.model
 
+import java.math.RoundingMode
+
 class RestaurantCalculator{
     fun calculateTip(checkAmount:Double,tipPercentage :Int):TipCalculation{
-        val tipAmount = checkAmount * (tipPercentage.toDouble()/100.0)
+        val tipAmount = (checkAmount * (tipPercentage.toDouble()/100.0))
+                .toBigDecimal()
+                .setScale(2,RoundingMode.HALF_UP)
+                .toDouble()
         val grandTotal = checkAmount + tipAmount
         return TipCalculation(checkAmount,
                 tipPercentage,
