a2a314b4081d8f54b862762cc13e36b209ac7358
sevenio <seveniotacheon@gmail.com>
2018-10-07 12:52:12 +0530

    add tip calculator activity test

---
+++ app/src/androidTest/java/com/example/gangadhar/tipcalculator/TipCalculatorActivityTest.kt
@@ -0,0 +1,14 @@
+package com.example.gangadhar.tipcalculator
+
+import android.support.test.rule.ActivityTestRule
+import com.example.gangadhar.tipcalculator.databinding.ActivityTipcalculatorBinding
+import com.example.gangadhar.tipcalculator.view.TipCalculatorActivity
+import org.junit.Rule
+
+class TipCalculatorActivityTest {
+
+    @get:Rule
+    var activityTestRule = ActivityTestRule(TipCalculatorActivity::class.java)
+
+
+}
